33bfac10f9a4e26b5a69b8587827f9077643080d
Avery Broderick <avery.c.broderick@gmail.com>
2018-03-13 14:40:11 +0000

    working with nokogiri

--- lib/good_quotes/cli.rb
+++ lib/good_quotes/cli.rb
@@ -1,6 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
+
 class GoodQuotes::CLI
   def call
     puts "Welcome to GoodQuotes"
+    list_quotes
   end
+
+  def list_quotes()
+    brainyquote = Nokogiri::HTML(open("https://www.brainyquote.com/"))
+    quotesList = brainyquote.css("#quotesList")
+    puts quotesList.css("a").text
+
+  end
+
 
 end
