7dc99d03aad6dc39a71e251d3bbd9c20b6349e04
Avery Broderick <avery.c.broderick@gmail.com>
2018-03-13 15:49:38 +0000

    working with loop

--- lib/good_quotes/cli.rb
+++ lib/good_quotes/cli.rb
@@ -3,15 +3,29 @@ require 'open-uri'
 
 class GoodQuotes::CLI
   def call
-    puts "Welcome to GoodQuotes"
+    puts "Welcome to GoodQuotes. Here are today's Top Ten Trending Quotes: "
     list_quotes
+    menu
   end
 
-  def list_quotes()
+  def list_quotes
     brainyquote = Nokogiri::HTML(open("https://www.brainyquote.com/"))
-    quotesList = brainyquote.css("#quotesList")
-    puts quotesList.css("a").text
+    quotesList = brainyquote.css("#quotesList img")
+    counter = 1
+    quotesList.each do |quote|
+      while counter <= 10
+        puts "#{counter}. " + quote.attribute("alt").value
+        counter += 1
+      end
+    end
+  end
 
+  def menu
+    input = nil
+    puts "Type in the quote number to learn more about the author, or type 'exit' to end current session."
+    until input == "exit"
+      input = gets.strip
+    end
   end
 
 
