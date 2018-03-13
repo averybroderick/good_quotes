5ee493953baf3d13b42c065014c93a148b3b1847
Avery Broderick <avery.c.broderick@gmail.com>
2018-03-13 17:25:24 +0000

    specified an environment file to link all files

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,8 @@
 require_relative "./good_quotes/version"
 require_relative "./good_quotes/cli"
 require_relative "./good_quotes/quotes"
+require_relative "./good_quotes/authors"
+require_relative "./good_quotes/scraper"
 
 module GoodQuotes
   # Your code goes here...
